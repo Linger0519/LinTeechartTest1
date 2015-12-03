50f0d1ec329c9237756324e0f2f2789353dd7e9d
Linger0519 <694987487@qq.com>
2015-12-04 00:21:58 +0800

    Upload Project Files
    based on Teechart8.ocx

---
+++ MyTeechartTest1/MyTeechartTest1Dlg.cpp
@@ -0,0 +1,180 @@
+
+// MyTeechartTest1Dlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "MyTeechartTest1.h"
+#include "MyTeechartTest1Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+	enum { IDD = IDD_ABOUTBOX };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMyTeechartTest1Dlg 对话框
+
+
+
+
+CMyTeechartTest1Dlg::CMyTeechartTest1Dlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CMyTeechartTest1Dlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMyTeechartTest1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_TCHART1, m_Teechart1);
+}
+
+BEGIN_MESSAGE_MAP(CMyTeechartTest1Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CMyTeechartTest1Dlg 消息处理程序
+
+BOOL CMyTeechartTest1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+	//for(long i = 0;i<m_Teechart1.get_SeriesCount();i++)
+	//{
+	//	((CSeries)m_Teechart1.Series(i)).Clear();
+	//}
+
+	m_Teechart1.RemoveAllSeries();
+	
+	m_Teechart1.AddSeries(scArea);
+	linSeries = ((CSeries)m_Teechart1.Series(0));
+	linSeries.FillSampleValues(16);
+	linSeries.put_Title(CString("First Series"));
+	linSeries.put_Color(RGB(25,10,155));
+
+	m_Teechart1.AddSeries(scLine);
+	linSeries = ((CSeries)m_Teechart1.Series(1));
+	linSeries.FillSampleValues(16);
+	linSeries.put_Title(CString("Second Series"));
+	linSeries.put_Color(RGB(125,210,15));
+
+	//m_Teechart1.Series(0).FillSampleValues(16);//显示横坐标个数
+	//m_Teechart1.Series(0).SetTitle("Area Series");
+	//m_Teechart1.AddSeries(scLine);	
+	//m_Teechart1.Series(1).FillSampleValues(16);
+	//m_Teechart1.Series(1).SetTitle("Line Series");
+	//m_Teechart1.AddSeries(scBar);	
+	//m_Teechart1.Series(2).FillSampleValues(16);
+	//m_Teechart1.Series(2).SetTitle("Bar Series");//设定各种series
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CMyTeechartTest1Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CMyTeechartTest1Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CMyTeechartTest1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
